0d9f1313f4850114f0744b7d62bdfb33c661d7d7
hienhong <hienhong@bravesoft.com.vn>
2022-08-15 19:37:41 +0900

    fix text color

--- src/components/Name.vue
+++ src/components/Name.vue
@@ -6,11 +6,11 @@
         <div class="flex items-center justify-center mt-4 space-x-4">
           <transition name="fade" mode="out-in">
             <div @click="nameToggle = !nameToggle" v-if="nameToggle" class="text-4xl align-middle cursor-pointer">
-              <a class="font-bold dark:text-slate-300 mr-2">Hồng Chấn</a>
-              <a class="font-extrabold dark:text-slate-200">Hiển</a>
+              <a class="font-bold text-slate-300 mr-2">Hồng Chấn</a>
+              <a class="font-extrabold text-slate-200">Hiển</a>
             </div>
             <div @click="nameToggle = !nameToggle" v-else class="text-4xl align-middle cursor-pointer">
-              <a class="font-bold dark:text-slate-300 mr-2">ホン チャン ヒエン</a>
+              <a class="font-bold text-slate-300 mr-2">ホン チャン ヒエン</a>
             </div>
           </transition>
           <a>
@@ -18,7 +18,7 @@
           </a>
         </div>
         <div>
-          <p class="text-lg dark:text-slate-400 py-1">
+          <p class="text-lg text-slate-400 py-1">
             Junior Web Developer in 東京都
           </p>
           <div class="block mx-auto align-center justify-left space-x-4 sm:mx-0 sm:shrink-0">
@@ -27,7 +27,7 @@
             <div class="h-7 inline-flex align-middle">
               <img @click="showMail = !showMail" class="rounded-md cursor-pointer transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300" src="../assets/mail.svg">
               <transition name="slide-fade">
-                <input v-if="showMail" type="text" class="ml-3 bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 cursor-not-allowed dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" value="chanhien050@gmail.com" disabled>
+                <input v-if="showMail" type="text" class="ml-3 bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 cursor-not-allowed dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" value="chanhien050@gmail.com" disabled>
               </transition>
             </div>
           </div>
--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -3,43 +3,43 @@
         <ol class="relative border-l border-gray-200 dark:border-gray-700">                  
             <li class="mb-10 ml-8">            
                 <span class="flex absolute -left-3 justify-center items-center w-6 h-6 bg-blue-200 rounded-full ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
-                    <svg aria-hidden="true" class="w-3 h-3 text-blue-600 dark:text-blue-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
+                    <svg aria-hidden="true" class="w-3 h-3 text-blue-600 text-blue-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
                 </span>
-                <h3 class="mb-1 text-lg font-semibold text-gray-900 dark:text-white">TOEIC Listening and Reading</h3>
-                <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">September 2017</time>
-                <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">Score: 910</p>  
+                <h3 class="mb-1 text-lg font-semibold text-gray-900 text-white">TOEIC Listening and Reading</h3>
+                <time class="block mb-2 text-sm font-normal leading-none text-gray-400 text-gray-500">September 2017</time>
+                <p class="mb-4 text-base font-normal text-gray-500 text-gray-400">Score: 910</p>  
             </li>
             <li class="mb-10 ml-8">            
                 <span class="flex absolute -left-3 justify-center items-center w-6 h-6 bg-blue-200 rounded-full ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
-                    <svg aria-hidden="true" class="w-3 h-3 text-blue-600 dark:text-blue-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
+                    <svg aria-hidden="true" class="w-3 h-3 text-blue-600 text-blue-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
                 </span>
-                <h3 class="mb-1 text-lg font-semibold text-gray-900 dark:text-white">Japanese Language Proficiency Test N2</h3>
-                <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">January 2021</time>
-                <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">Passed</p>  
+                <h3 class="mb-1 text-lg font-semibold text-gray-900 text-white">Japanese Language Proficiency Test N2</h3>
+                <time class="block mb-2 text-sm font-normal leading-none text-gray-400 text-gray-500">January 2021</time>
+                <p class="mb-4 text-base font-normal text-gray-500 text-gray-400">Passed</p>  
             </li>
             <li class="mb-10 ml-8">            
                 <span class="flex absolute -left-3 justify-center items-center w-6 h-6 bg-blue-200 rounded-full ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
-                    <svg aria-hidden="true" class="w-3 h-3 text-blue-600 dark:text-blue-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
+                    <svg aria-hidden="true" class="w-3 h-3 text-blue-600 text-blue-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
                 </span>
-                <h3 class="mb-1 text-lg font-semibold text-gray-900 dark:text-white">Intern at Bravesoft Vietnam</h3>
-                <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">August 2021</time>
-                <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">Fullstack Developer</p>  
+                <h3 class="mb-1 text-lg font-semibold text-gray-900 text-white">Intern at Bravesoft Vietnam</h3>
+                <time class="block mb-2 text-sm font-normal leading-none text-gray-400 text-gray-500">August 2021</time>
+                <p class="mb-4 text-base font-normal text-gray-500 text-gray-400">Fullstack Developer</p>  
             </li>
             <li class="mb-10 ml-8">            
                 <span class="flex absolute -left-3 justify-center items-center w-6 h-6 bg-blue-200 rounded-full ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
-                    <svg aria-hidden="true" class="w-3 h-3 text-blue-600 dark:text-blue-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
+                    <svg aria-hidden="true" class="w-3 h-3 text-blue-600 text-blue-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
                 </span>
-                <h3 class="mb-1 text-lg font-semibold text-gray-900 dark:text-white">Graduated University of Information Technology</h3>
-                <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">October 2021</time>
-                <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">Bachelor in Information Security</p>  
+                <h3 class="mb-1 text-lg font-semibold text-gray-900 text-white">Graduated University of Information Technology</h3>
+                <time class="block mb-2 text-sm font-normal leading-none text-gray-400 text-gray-500">October 2021</time>
+                <p class="mb-4 text-base font-normal text-gray-500 text-gray-400">Bachelor in Information Security</p>  
             </li>
             <li class="mb-10 ml-8">            
                 <span class="flex absolute -left-3 justify-center items-center w-6 h-6 bg-blue-200 rounded-full ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
-                    <svg aria-hidden="true" class="w-3 h-3 text-blue-600 dark:text-blue-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
+                    <svg aria-hidden="true" class="w-3 h-3 text-blue-600 text-blue-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
                 </span>
-                <h3 class="mb-1 text-lg font-semibold text-gray-900 dark:text-white">Official employee at Nissho Electronics</h3>
-                <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">April 2022</time>
-                <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">Network Administrator</p>  
+                <h3 class="mb-1 text-lg font-semibold text-gray-900 text-white">Official employee at Nissho Electronics</h3>
+                <time class="block mb-2 text-sm font-normal leading-none text-gray-400 text-gray-500">April 2022</time>
+                <p class="mb-4 text-base font-normal text-gray-500 text-gray-400">Network Administrator</p>  
             </li>
         </ol>
     </div>
